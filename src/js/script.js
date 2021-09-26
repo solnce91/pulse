65ea0cdd2c264d528e6a0dfbfbd69e745b323951
olga <astafevaos2106@gmail.com>
2021-09-26 14:03:18 +1000

    add php

--- src/js/script.js
+++ src/js/script.js
@@ -72,5 +72,21 @@ $(document).ready(function(){
     valideForms('#consultation-form');
     valideForms('#consultation form');
     valideForms('#order form');
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn();
+        $('form').trigger('reset');
+      });
+      return false;
+    });
 });
   
